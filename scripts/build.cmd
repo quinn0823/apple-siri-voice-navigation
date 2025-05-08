0448876f84876007c284f25ead3d8eb42b91148f
Jonathan Chiu <quinnqiu0823@icloud.com>
2025-05-08 16:03:05 +0800

    Create build scripts

---
+++ scripts/build.cmd
@@ -0,0 +1,5 @@
+@REM Apple Siri Voice Navigation
+@REM The MIT License (MIT)
+@REM Copyright (c) 2025 Jonathan Chiu
+
+@REM Under Development...
---
+++ scripts/build.sh
@@ -0,0 +1,5 @@
+# Apple Siri Voice Navigation
+# The MIT License (MIT)
+# Copyright (c) 2025 Jonathan Chiu
+
+# Under Development...
