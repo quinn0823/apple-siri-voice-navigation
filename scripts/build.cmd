142b14930c5b144a4bd1ed6d69e0a1a86a2f9cbf
Jonathan Chiu <quinnqiu0823@icloud.com>
2026-05-16 22:26:55 +0800

    Delete the legacy scripts

--- scripts/build.cmd
+++
@@ -1,5 +0,0 @@
-@REM Apple Siri Voice Navigation
-@REM The GNU General Public License v3.0 (GPL-3.0)
-@REM Copyright (c) 2025 Jonathan Chiu
-
-@REM Under Development...
--- scripts/build.sh
+++
@@ -1,5 +0,0 @@
-# Apple Siri Voice Navigation
-# The GNU General Public License v3.0 (GPL-3.0)
-# Copyright (c) 2025 Jonathan Chiu
-
-# Under Development...
